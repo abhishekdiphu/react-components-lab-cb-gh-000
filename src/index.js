d3546efded78130a17491b0db067b4b163679f52
Abhishek Buragohain <abhishekdiphu@gmail.com>
2019-01-23 02:48:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ export class InFrontOfYou extends Component {
     return (
       <div class= "InrontOfYou" >
           <p>You shouldnt look too far.</p>
-          <p>Sometimes, the solution is right in front of you.</p>
+          <p1>Sometimes, the solution is right in front of you.</p1>
       </div>
 );
 
